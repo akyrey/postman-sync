b7bb097a30ea1a4895557047f4d06d336d8e233c
akyrey <stein.dario@gmail.com>
2023-08-18 23:01:13 +0200

    feat: update models and postman requests
    Create all models and requests to perform a sync: workspace, collection
    and environment
    Environment model isn't complete, but we don't mind at the moment, since
    will save the retrieved json to use it to create/update another one

--- pkg/postman/models.go
+++ pkg/postman/models.go
@@ -2,47 +2,127 @@ package postman
 
 import "time"
 
-type Workspace struct {
-	ID           string        `json:"id"`
-	Name         string        `json:"name"`
-	Type         string        `json:"type"`
-	Description  string        `json:"description"`
-	Visibility   string        `json:"visibility"`
-	CreatedBy    string        `json:"createdBy"`
-	UpdatedBy    string        `json:"updatedBy"`
-	CreatedAt    time.Time     `json:"createdAt"`
-	UpdatedAt    time.Time     `json:"updatedAt"`
-	Collections  []Collection  `json:"collections"`
-	Environments []Environment `json:"environments"`
-	Mocks        []Mock        `json:"mocks"`
-	Apis         []Api         `json:"apis"`
+type WorkspaceVisibility string
+
+const (
+	Team     WorkspaceVisibility = "team"
+	Personal WorkspaceVisibility = "personal"
+	Public   WorkspaceVisibility = "public"
+)
+
+type WorkspaceInfo struct {
+	ID         string              `json:"id"`
+	Name       string              `json:"name"`
+	Type       string              `json:"type"`
+	Visibility WorkspaceVisibility `json:"visibility"`
 }
 type WorkspaceListResponse struct {
-	Workspaces []Workspace `json:"workspaces"`
+	Workspaces []WorkspaceInfo `json:"workspaces"`
+}
+
+type Workspace struct {
+	ID           string            `json:"id"`
+	Name         string            `json:"name"`
+	Type         string            `json:"type"`
+	Description  string            `json:"description"`
+	Visibility   string            `json:"visibility"`
+	CreatedBy    string            `json:"createdBy"`
+	UpdatedBy    string            `json:"updatedBy"`
+	CreatedAt    time.Time         `json:"createdAt"`
+	UpdatedAt    time.Time         `json:"updatedAt"`
+	Collections  []CollectionInfo  `json:"collections"`
+	Environments []EnvironmentInfo `json:"environments"`
+	Mocks        []MockInfo        `json:"mocks"`
+	Apis         []ApiInfo         `json:"apis"`
 }
 type WorkspaceResponse struct {
 	Workspace Workspace `json:"workspace"`
 }
 
+type CollectionInfo struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Owner     string    `json:"owner"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	UID       string    `json:"uid"`
+	IsPublic  bool      `json:"isPublic"`
+}
+type CollectionListResponse struct {
+	Collections []CollectionInfo `json:"collections"`
+}
+
 type Collection struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	UID  string `json:"uid"`
+	Info  Info    `json:"info"`
+	Item  []Item  `json:"item"`
+	Auth  Auth    `json:"auth"`
+	Event []Event `json:"event"`
+}
+type Info struct {
+	PostmanID   string    `json:"_postman_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Schema      string    `json:"schema"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	UID         string    `json:"uid"`
 }
 
+// TODO: incomplete
+type Item struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+	Item []Item
+}
+
+// TODO: incomplete
+type Auth struct {
+	Type string `json:"type"`
+}
+
+// TODO: incomplete
+type Event struct{}
+
+type CollectionResponse struct {
+	Collection Collection `json:"collection"`
+}
+
+type EnvironmentInfo struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Owner     string    `json:"owner"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	UID       string    `json:"uid"`
+	IsPublic  bool      `json:"isPublic"`
+}
+type EnvironmentListResponse struct {
+	Environments []EnvironmentInfo `json:"environments"`
+}
 type Environment struct {
+	ID        string             `json:"id"`
+	Name      string             `json:"name"`
+	Owner     string             `json:"owner"`
+	CreatedAt time.Time          `json:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt"`
+	Values    []EnvironmentValue `json:"values"`
+	IsPublic  bool               `json:"isPublic"`
+}
+type EnvironmentValue struct {
+	Key     string `json:"key"`
+	Value   string `json:"value"`
+	Enabled bool   `json:"enabled"`
+}
+type EnvironmentResponse struct {
+	Environment Environment `json:"environment"`
+}
+
+type MockInfo struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	UID  string `json:"uid"`
 }
 
-type Mock struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	UID  string `json:"uid"`
-}
-
-type Api struct {
+type ApiInfo struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	UID  string `json:"uid"`
--- pkg/postman/postman.go
+++ pkg/postman/postman.go
@@ -29,7 +29,7 @@ type Postman struct {
 /*************************************************************************************************/
 /******************************************* WORSPACE ********************************************/
 /*************************************************************************************************/
-func (p *Postman) RetrieveWorkspaces() ([]Workspace, error) {
+func (p *Postman) RetrieveWorkspaces() ([]WorkspaceInfo, error) {
 	body, err := p.performRequest("GET", fmt.Sprintf("%sworkspaces/", url), nil)
 	if err != nil {
 		return nil, err
@@ -42,7 +42,7 @@ func (p *Postman) RetrieveWorkspaces() ([]Workspace, error) {
 		return nil, err
 	}
 
-	return resp.Workspaces, err
+	return resp.Workspaces, nil
 }
 
 func (p *Postman) RetrieveWorkspace(id string) (*Workspace, error) {
@@ -58,7 +58,7 @@ func (p *Postman) RetrieveWorkspace(id string) (*Workspace, error) {
 		return nil, err
 	}
 
-	return &resp.Workspace, err
+	return &resp.Workspace, nil
 }
 
 func (p *Postman) CreateWorkspace(ws Workspace) (*Workspace, error) {
@@ -78,15 +78,15 @@ func (p *Postman) CreateWorkspace(ws Workspace) (*Workspace, error) {
 		return nil, err
 	}
 
-	return &workspace, err
+	return &workspace, nil
 }
 
-func (p *Postman) UpdateWorkspace(id string, ws Workspace) (*Workspace, error) {
+func (p *Postman) UpdateWorkspace(uid string, ws Workspace) (*Workspace, error) {
 	reqBody, err := json.Marshal(WorkspaceResponse{Workspace: ws})
 	if err != nil {
 		return nil, err
 	}
-	body, err := p.performRequest("PUT", fmt.Sprintf("%sworkspaces/%s", url, id), reqBody)
+	body, err := p.performRequest("PUT", fmt.Sprintf("%sworkspaces/%s", url, uid), reqBody)
 	if err != nil {
 		return nil, err
 	}
@@ -98,7 +98,7 @@ func (p *Postman) UpdateWorkspace(id string, ws Workspace) (*Workspace, error) {
 		return nil, err
 	}
 
-	return &workspace, err
+	return &workspace, nil
 }
 
 func (p *Postman) DeleteWorkspace(id string) error {
@@ -113,114 +113,170 @@ func (p *Postman) DeleteWorkspace(id string) error {
 /*************************************************************************************************/
 /***************************************** COLLECTION ********************************************/
 /*************************************************************************************************/
-// func (p *Postman) RetrieveCollections() {
-// 	body, err := p.performRequest("GET", fmt.Sprintf("%scollections", url), nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) RetrieveCollection(id string) {
-// 	body, err := p.performRequest("GET", fmt.Sprintf("%scollections/%s", url, id), nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) CreateCollections(collection Collection) {
-// 	reqBody, err := json.Marshal(collection)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	body, err := p.performRequest("POST", fmt.Sprintf("%scollections", url), reqBody)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) UpdateCollection(id string, collection Collection) {
-// 	reqBody, err := json.Marshal(collection)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	body, err := p.performRequest("PUT", fmt.Sprintf("%scollections/%s", url, id), reqBody)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) DeleteCollection(id string) {
-// 	body, err := p.performRequest("DELETE", fmt.Sprintf("%scollections/%s", url, id), nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
+func (p *Postman) RetrieveCollections() ([]CollectionInfo, error) {
+	body, err := p.performRequest("GET", fmt.Sprintf("%scollections", url), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var resp CollectionListResponse
+	err = json.NewDecoder(body).Decode(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Collections, nil
+}
+
+func (p *Postman) RetrieveCollection(uid string) (*Collection, error) {
+	body, err := p.performRequest("GET", fmt.Sprintf("%scollections/%s", url, uid), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var resp CollectionResponse
+	err = json.NewDecoder(body).Decode(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.Collection, nil
+}
+
+func (p *Postman) CreateCollections(c Collection, wsUid string) (*Collection, error) {
+	reqBody, err := json.Marshal(CollectionResponse{Collection: c})
+	if err != nil {
+		return nil, err
+	}
+	body, err := p.performRequest("POST", fmt.Sprintf("%scollections?workspace=%s", url, wsUid), reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var collection Collection
+	err = json.NewDecoder(body).Decode(&collection)
+	if err != nil {
+		return nil, err
+	}
+
+	return &collection, nil
+}
+
+func (p *Postman) UpdateCollection(uid string, c Collection) (*Collection, error) {
+	reqBody, err := json.Marshal(CollectionResponse{Collection: c})
+	if err != nil {
+		return nil, err
+	}
+	body, err := p.performRequest("PUT", fmt.Sprintf("%scollections/%s", url, uid), reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var collection Collection
+	err = json.NewDecoder(body).Decode(&collection)
+	if err != nil {
+		return nil, err
+	}
+
+	return &collection, nil
+}
+
+func (p *Postman) DeleteCollection(id string) error {
+	_, err := p.performRequest("DELETE", fmt.Sprintf("%scollections/%s", url, id), nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // /*************************************************************************************************/
 // /**************************************** ENVIRONMENT ********************************************/
 // /*************************************************************************************************/
-// func (p *Postman) RetrieveEnvironments() {
-// 	body, err := p.performRequest("GET", fmt.Sprintf("%senvironments", url), nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) RetrieveEnvironment(id string) {
-// 	body, err := p.performRequest("GET", fmt.Sprintf("%senvironments/%s", url, id), nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) CreateEnvironments(environment Environment) {
-// 	reqBody, err := json.Marshal(environment)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	body, err := p.performRequest("POST", fmt.Sprintf("%senvironments", url), reqBody)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) UpdateEnvironment(id string, environment Environment) {
-// 	reqBody, err := json.Marshal(environment)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	body, err := p.performRequest("PUT", fmt.Sprintf("%senvironments/%s", url, id), reqBody)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
-//
-// func (p *Postman) DeleteEnvironment(id string) {
-// 	body, err := p.performRequest("DELETE", fmt.Sprintf("%senvironments/%s", url, id), nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	fmt.Println(string(body))
-// }
+func (p *Postman) RetrieveEnvironments() ([]EnvironmentInfo, error) {
+	body, err := p.performRequest("GET", fmt.Sprintf("%senvironments", url), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var resp EnvironmentListResponse
+	err = json.NewDecoder(body).Decode(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Environments, nil
+}
+
+func (p *Postman) RetrieveEnvironment(uid string) (*Environment, error) {
+	body, err := p.performRequest("GET", fmt.Sprintf("%senvironments/%s", url, uid), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var resp EnvironmentResponse
+	err = json.NewDecoder(body).Decode(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.Environment, nil
+}
+
+func (p *Postman) CreateEnvironments(e Environment, wsUid string) (*Environment, error) {
+	reqBody, err := json.Marshal(EnvironmentResponse{Environment: e})
+	if err != nil {
+		return nil, err
+	}
+	body, err := p.performRequest("POST", fmt.Sprintf("%senvironments?workspace=%s", url, wsUid), reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var environment Environment
+	err = json.NewDecoder(body).Decode(&environment)
+	if err != nil {
+		return nil, err
+	}
+
+	return &environment, nil
+}
+
+func (p *Postman) UpdateEnvironment(uid string, e Environment) (*Environment, error) {
+	reqBody, err := json.Marshal(EnvironmentResponse{Environment: e})
+	if err != nil {
+		return nil, err
+	}
+	body, err := p.performRequest("PUT", fmt.Sprintf("%senvironments/%s", url, uid), reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	defer body.Close()
+	var environment Environment
+	err = json.NewDecoder(body).Decode(&environment)
+	if err != nil {
+		return nil, err
+	}
+
+	return &environment, nil
+}
+
+func (p *Postman) DeleteEnvironment(uid string) error {
+	_, err := p.performRequest("DELETE", fmt.Sprintf("%senvironments/%s", url, uid), nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Method used to perform all fetch requests
 func (p *Postman) performRequest(method, url string, reqBody []byte) (io.ReadCloser, error) {
